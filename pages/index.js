4cd2bb9cd92d5e6dd4f5e90fc29bfc87a754112b
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2021-04-18 21:49:13 -0300

    px-40

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
       </Head>
 
 
-      
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+
+      <main className="flex flex-col items-center justify-center flex-1 px-40 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
